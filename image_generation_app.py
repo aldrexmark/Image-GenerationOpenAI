7b1a465bdca7f471f6710f166a56afc18ecdaecd
aldrexmark <114469611+aldrexmark@users.noreply.github.com>
2023-04-24 22:12:28 +0800

    Update image_generation_app.py

--- image_generation_app.py
+++ image_generation_app.py
@@ -10,8 +10,7 @@ from requests.structures import CaseInsensitiveDict
 import streamlit as st
 
 #API KEY
-OPENAI_API_KEY=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
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = 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
 openai.__version__ = "0.10.2"
 
 #HEADER
